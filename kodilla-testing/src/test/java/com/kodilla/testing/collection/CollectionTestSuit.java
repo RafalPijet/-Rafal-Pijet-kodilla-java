680e1f7070a29df76ade440a7e223b0710d1c306
RafalPijet <stronglopez@wp.pl>
2018-01-12 20:29:21 +0100

    Moduł 6.3 zadanie_poprawka1

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuit.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuit.java
@@ -5,22 +5,29 @@ import org.junit.*;
 import java.util.ArrayList;
 
 public class CollectionTestSuit {
+    private static int counterTest = 0;
+
     @Before
     public void before(){
-        System.out.println("\nRozpoczęcie przypadku testowego");
+        counterTest++;
+        System.out.println("\nRozpoczęcie przypadku testowego nr " + counterTest);
     }
+
     @After
     public void after() {
-        System.out.println("Zakończenie przypadku testowego");
+        System.out.println("Zakończenie przypadku testowego nr " + counterTest);
     }
+
     @BeforeClass
     public static void beforeClass() {
         System.out.println("POCZĄTEK ZESTAWU TESTÓW");
     }
+
     @AfterClass
     public static void afterClass() {
         System.out.println("\nKONIEC ZESTAWU TESTÓW");
     }
+
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         //Given
@@ -29,20 +36,29 @@ public class CollectionTestSuit {
         //When
         ArrayList<Integer> checkList = oddNumberExterminator.exterminate(emptyList);
         //Then
-        Assert.assertEquals(emptyList.size(), checkList.size());
+        Assert.assertTrue(checkList.isEmpty());
     }
+
     @Test
     public void testOddNumberExterminatorNormalList() {
+
         //Given
+        OddNumbersExterminator oddNumberExterminator = new OddNumbersExterminator();
+        ArrayList<Integer> onlyEvenNumbersList = new ArrayList<Integer>();
         ArrayList<Integer> numbersList = new ArrayList<Integer>();
+        for (int i = 2; i < 11; i += 2) {
+            onlyEvenNumbersList.add(i);
+        }
         for (int i = 1; i < 11; i++) {
             numbersList.add(i);
         }
-        OddNumbersExterminator oddNumberExterminator = new OddNumbersExterminator();
+
         //When
         ArrayList<Integer> checkList = oddNumberExterminator.exterminate(numbersList);
+
         //Then
-        Assert.assertEquals(numbersList.get(1), checkList.get(0));
-        Assert.assertEquals(numbersList.get(9), checkList.get(4));
+        for (int i = 0; i < checkList.size(); i++) {
+            Assert.assertEquals(onlyEvenNumbersList.get(i), checkList.get(i));
+        }
     }
 }
