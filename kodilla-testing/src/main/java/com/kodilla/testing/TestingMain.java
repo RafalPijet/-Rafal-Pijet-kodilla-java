a70ee701be9f73a95d8512fb2e9e025d9c8e42ad
RafalPijet <stronglopez@wp.pl>
2018-01-12 08:43:39 +0100

    Moduł 6.2 zadanie_poprawka1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -2,47 +2,19 @@ package com.kodilla.testing;
 
 import com.kodilla.testing.caclulator.Calculator;
 
-import java.util.Random;
-
 public class TestingMain {
     public static void main(String[] args) {
-        boolean isAddOk = false;
-        boolean isSubOk = false;
-        int loopCount = 1000;
-        Random randGen = new Random();
-        int randNumb[] = new int [loopCount];
-        for (int i = 0; i < loopCount; i++) {
-            randNumb[i] = randGen.nextInt(1000);
-        }
         Calculator calculator = new Calculator();
         System.out.println("Rozpoczęcie wykonywania testów dla dwóch metod 'add()' i 'subtract()' klasy 'Calculator'");
-        System.out.println("Ilość operacji testowych --> " + loopCount + " par argumentów");
-        for (int i = 0; i < randNumb.length; i++) {
-            if ((calculator.add(randNumb[i], randNumb[randNumb.length - i - 1])) == (randNumb[i] + randNumb[randNumb.length - i - 1])) {
-                isAddOk = true;
-            } else {
-                isAddOk = false;
-                break;
-            }
-        }
-        for (int i = 0; i < randNumb.length; i++) {
-            if ((calculator.subtract(randNumb[i], randNumb[randNumb.length - i - 1])) == (randNumb[i] - randNumb[randNumb.length - i - 1])) {
-                isSubOk = true;
-            } else {
-                isSubOk = false;
-                break;
-            }
-        }
-        if (isAddOk) {
+        if (calculator.add(75,25) == 100) {
             System.out.println("\nTest jednostkowy dla metody 'add()' zakończony SUKCESEM");
         } else {
             System.out.println("\nPodczas testu jednostkowego dla metody 'add()' wystąpił Błąd!!!");
         }
-        if (isSubOk) {
+        if (calculator.subtract(75,25) == 50) {
             System.out.println("\nTest jednostkowy dla metody 'subtract()' zakończony SUKCESEM");
         } else {
             System.out.println("\nPodczas testu jednostkowego dla metody 'subtract()' wystąpił Błąd!!!");
         }
-
     }
 }
