1af6df25c679c5948845cab9003cb24c5a94dcfb
RafalPijet <stronglopez@wp.pl>
2018-01-14 18:54:40 +0100

    Moduł 6.5 zadanie

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -8,16 +8,41 @@ public class ForumStatistics {
     private double averageCommentsAnUser;
     private double averageCommentsAnPost;
 
-    public ForumStatistics(Statistics statistics) {
-
-    }
-
     public void calculateAdvStatistics(Statistics statistics) {
-
+        usersQuantity = statistics.usersNames().size();
+        postsQuantity = statistics.postsCount();
+        commentsQuantity = statistics.commentsCount();
+        if (postsQuantity != 0 && usersQuantity != 0 ) {
+            averagePostsAnUser = (double) postsQuantity / usersQuantity;
+        }
+        if (commentsQuantity != 0 && usersQuantity != 0) {
+            averageCommentsAnUser = (double) commentsQuantity / usersQuantity;
+        }
+        if (commentsQuantity != 0 && postsQuantity != 0) {
+            averageCommentsAnPost = (double) commentsQuantity / postsQuantity;
+        }
     }
 
     public void showStatistics() {
+        System.out.println("Ilość użytkowników --> " + usersQuantity);
+        System.out.println("Ilość postów --> " + postsQuantity);
+        System.out.println("Ilość komentarzy --> " + commentsQuantity);
+        System.out.printf("Średnia ilość postów na użytkownika --> " + "%5.2f", averagePostsAnUser);
+        System.out.printf("\nŚrednia ilość komentarzy na użytkownika --> " + "%5.2f", averageCommentsAnUser);
+        System.out.printf("\nŚrednia ilość komentarzy na post --> " + "%5.2f", averageCommentsAnPost);
+        System.out.println();
 
     }
 
+    public int getUsersQuantity() {
+        return usersQuantity;
+    }
+
+    public int getPostsQuantity() {
+        return postsQuantity;
+    }
+
+    public int getCommentsQuantity() {
+        return commentsQuantity;
+    }
 }
