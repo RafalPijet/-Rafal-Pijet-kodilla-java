36f6a76ca27cc7b1e3ba30fb04fdef067b7f3686
RafalPijet <stronglopez@wp.pl>
2018-01-18 20:09:39 +0100

    Moduł 7.1 zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,91 +1,19 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
-import com.kodilla.stream.person.People;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-/*
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
-*/
 public class StreamMain {
     public static void main(String[] args){
-        BookDirectory theBookDirectory = new BookDirectory();
-        String theResultStringOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .map(Book::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
-        System.out.println(theResultStringOfBooks);
-
-
-        /*
-        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toMap(Book::getSignature, book -> book));
-        System.out.println("# elements: " + theResultMapOfBooks.size());
-        theResultMapOfBooks.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .forEach(System.out::println);
-
-        /*
-        BookDirectory bookDirectory = new BookDirectory();
-        List<Book> theResultListOfBooks = bookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toList());
-        System.out.println("# elements: " + theResultListOfBooks.size());
-        theResultListOfBooks.stream()
-                .forEach(System.out::println);
-        /*
-
-
-        People.getList().stream()
-                .map(s -> s.toUpperCase())
-                .forEach(System.out::println);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String exampleText = "Jestem sobie wesoły Romek i mam na przedmieściu domek.";
+        System.out.println("Oryginalny tekst: " + exampleText);
         System.out.println();
-        People.getList().stream()
-                .map(String::toUpperCase)
-                .forEach(s -> System.out.println(s));
-        System.out.println();
-        People.getList().stream()
-                .filter(s -> s.length() > 11)
-                .forEach(System.out::println);
-        System.out.println();
-        People.getList().stream()
-                .map(s -> s.toUpperCase())//.map(String::toUpperCase) <-- alternatywa
-                .filter(s -> s.length() > 11)
-                .map(s -> s.substring(0,s.indexOf(' ') + 2) + ".")
-                .filter(s -> s.substring(0,1).equals("M"))
-                .forEach(System.out::println);
-                */
+        poemBeautifier.beautify(exampleText, text -> text.toUpperCase());
+        poemBeautifier.beautify(exampleText, text -> text.toLowerCase());
+        poemBeautifier.beautify(exampleText, text -> text.concat("\b\b\b\b\b\b\b chatę."));
+        poemBeautifier.beautify(exampleText, text -> text.substring(13, 25).concat(" ma domek"));
+        poemBeautifier.beautify(exampleText, text -> text.replace("Jestem", "Byłem").replace("mam", "miałem"));
+        poemBeautifier.beautify(exampleText, text -> text.replaceFirst("m", "ś").substring(0, 28).concat("już!"));
 
-
-   /*     System.out.println("Welcome to module 7 - Stream");
-        SaySomething saySomething = new SaySomething();
-        saySomething.say();
-        Processor processor = new Processor();
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        processor.execute(executeSaySomething);
-        Executor codeToExecute = () -> System.out.println("This is an third example text.");
-        processor.execute(codeToExecute);
-        processor.execute(() -> System.out.println("This is an fourth example text"));
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        System.out.println("\nCalculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-        System.out.println("\nCalculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subAfromB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-        System.out.println();
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);*/
     }
 }
