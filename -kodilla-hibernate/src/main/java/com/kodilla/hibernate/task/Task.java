ecb32293b8bf943e1038532c1f5d36ed4fd09d21
RafalPijet <stronglopez@wp.pl>
2018-03-02 17:50:50 +0100

    Moduł 13.2 przykład

--- -kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ -kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,8 +1,6 @@
 package com.kodilla.hibernate.task;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
@@ -14,12 +12,19 @@ public class Task {
     private Date created;
     private int duration;
 
+    public Task() {
+
+    }
+
     public Task(String description, int duration) {
         this.description = description;
         this.created = new Date();
         this.duration = duration;
     }
-
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
     public int getId() {
         return id;
     }
@@ -29,7 +34,7 @@ public class Task {
         return description;
     }
     @NotNull
-    @Column(name = "CREATE")
+    @Column(name = "CREATED")
     public Date getCreated() {
         return created;
     }
@@ -37,5 +42,21 @@ public class Task {
     @Column(name = "DURATION")
     public int getDuration() {
         return duration;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    private void setCreated(Date created) {
+        this.created = created;
+    }
+
+    private void setDuration(int duration) {
+        this.duration = duration;
     }
 }
