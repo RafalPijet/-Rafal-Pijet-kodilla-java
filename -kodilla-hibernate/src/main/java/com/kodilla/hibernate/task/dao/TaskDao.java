6a852995de7e0c50527916a03fa8c0221dc2aec2
RafalPijet <stronglopez@wp.pl>
2018-03-06 10:37:53 +0100

    Moduł 13.4 przykład

--- -kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ -kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -1,7 +1,9 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +13,13 @@ import java.util.List;
 @Transactional
 public interface TaskDao extends CrudRepository<Task, Integer> {
     List<Task> findByDuration(int duration);
+
+    @Query
+    List<Task> retriveLongTasks();
+    @Query
+    List<Task> retriveShortTasks();
+    @Query(nativeQuery = true)
+    List<Task> retriveTasksWithEnoughTime();
+    @Query
+    List<Task> retriveTasksWithDurationLongerThan(@Param("DURATION") int duration);
 }
